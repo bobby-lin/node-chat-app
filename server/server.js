54188d2ed35f20982d229385fd509e78853a16bf
Bobby Lin <mr.linjiahao@gmail.com>
2017-05-24 12:01:42 +0800

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
+// https://www.npmjs.com/package/socket.io
+
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
-const app = express();
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  })
+});
+
+server.listen(port, () => {
   console.log(`Chat app listening on port ${port}!`);
 })
